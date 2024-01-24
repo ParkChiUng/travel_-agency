cee645774a2bf77b5092f07ba6cd7b27a1f91181
Yujin Choe <dev.yujinchoe@gmail.com>
2024-01-24 10:20:47 +0900

    feat & design & style : 숙소 페이지 구현

---
+++ app/src/main/java/com/sessac/travel_agency/LodgingAdapter.kt
@@ -0,0 +1,48 @@
+package com.sessac.travel_agency
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.sessac.travel_agency.data.LodgingItem
+
+class LodgingAdapter(private val lodgingList:ArrayList<LodgingItem>) : RecyclerView.Adapter<LodgingAdapter.LodgingHolder>() {
+
+    //var onItemClick : ((LodgingItem) -> Unit)? = null
+
+    inner class LodgingHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView : ImageView = itemView.findViewById(R.id.lodging_thumbnail)
+        val locationTextView : TextView = itemView.findViewById(R.id.lodging_location)
+        val nameTextView : TextView = itemView.findViewById(R.id.lodging_name)
+        val rateView : RatingBar = itemView.findViewById(R.id.lodging_rating)
+    }
+
+    // 뷰홀더와 그에 연결된 뷰를 생성하고 초기화
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LodgingHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_lodging, parent, false )
+        return LodgingHolder(view)
+    }
+
+    // 데이터세트 크기 가져옴
+    override fun getItemCount(): Int {
+        return lodgingList.size
+    }
+
+    // 뷰홀더를 데이터와 연결
+    override fun onBindViewHolder(holder: LodgingHolder, position: Int) {
+        val lodging = lodgingList[position]
+        holder.imageView.setImageResource(lodging.lImage)
+        holder.locationTextView.text = lodging.area
+        holder.nameTextView.text = lodging.lName
+        holder.rateView.rating = lodging.starNum.toFloat()
+
+        // 카드 클릭시
+//        holder.itemView.setOnClickListener {
+//            onItemClick?.invoke(lodging)
+//        }
+
+    }
+}
--- app/src/main/java/com/sessac/travel_agency/data/LodgingItem.kt
+++ app/src/main/java/com/sessac/travel_agency/data/LodgingItem.kt
@@ -1,6 +1,8 @@
 package com.sessac.travel_agency.data
 
 import android.graphics.Bitmap
+//import android.os.Parcelable
+//import kotlinx.parcelize.Parcelize
 
 /**
  * @param lodgeId : Auto Increment 사용하여 자동으로 증가 예정
@@ -9,10 +11,11 @@ import android.graphics.Bitmap
  * @param lImage : 숙소 이미지
  * @param starNum : 숙소 등급
  */
+//@Parcelize //gradle 플러그인에 parcelize 추가함. 데이터 넘기기 위해
 data class LodgingItem(
     val lodgeId : Int,
     val area : String,
     val lName : String,
-    val lImage : Bitmap,
+    val lImage : Int, // 샘플 Bitmap파일이 Int로 인식되어 잠시 Int로
     val starNum : Int
-)
+) //: Parcelable
