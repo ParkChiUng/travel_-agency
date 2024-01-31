2ef9d6de976d3f6a88ba88227fd6ebe5f63aea17
Yujin Choe <dev.yujinchoe@gmail.com>
2024-01-31 14:16:50 +0900

    chore: 뷰홀더 아이템 이름 수정 반영

--- app/src/main/java/com/sessac/travel_agency/adapter/PackageFragmentAdapter.kt
+++ app/src/main/java/com/sessac/travel_agency/adapter/PackageFragmentAdapter.kt
@@ -3,9 +3,9 @@ package com.sessac.travel_agency.adapter
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.sessac.travel_agency.fragment.PackageFragment
-import com.sessac.travel_agency.fragment.packageTap.EndedPackage
-import com.sessac.travel_agency.fragment.packageTap.OngoingPackage
-import com.sessac.travel_agency.fragment.packageTap.ScheduledPackage
+import com.sessac.travel_agency.fragment.packageTap.EndedPackageFragment
+import com.sessac.travel_agency.fragment.packageTap.OngoingPackageFragment
+import com.sessac.travel_agency.fragment.packageTap.ScheduledPackageFragment
 
 private const val NUM_PAGES = 3
 
@@ -18,10 +18,10 @@ class PackageFragmentAdapter(fragmentActivity: PackageFragment) :
 
     override fun createFragment(position: Int): Fragment {
         when(position) {
-            0 -> return EndedPackage()
-            1 -> return OngoingPackage()
-            2 -> return ScheduledPackage()
-            else -> return OngoingPackage()
+            0 -> return EndedPackageFragment()
+            1 -> return OngoingPackageFragment()
+            2 -> return ScheduledPackageFragment()
+            else -> return OngoingPackageFragment()
         }
     }
 
